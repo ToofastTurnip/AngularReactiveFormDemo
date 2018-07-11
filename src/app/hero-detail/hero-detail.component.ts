5d12ec3d45461b8ea4d4974b6d7c23d415ac4e64
Mitch <taylor.mitch.5@gmail.com>
2018-07-11 13:43:06 -0400

    Update/diagnostic

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component }              from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component }                          from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-detail',
@@ -35,7 +35,7 @@ export class HeroDetailComponent3 {
 
   createForm() {
     this.heroForm = this.fb.group({
-      name: ''
+      name: ['', Validators.required ],
     });
   }
 }
