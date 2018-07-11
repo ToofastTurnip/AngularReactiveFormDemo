430381539e2704fdd8c011b3b9f381b480ee1207
Mitch <taylor.mitch.5@gmail.com>
2018-07-11 14:12:05 -0400

    More form options but most don't work lol

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component }                          from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { states } from '../data-model';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -26,8 +28,23 @@ export class HeroDetailComponent {
 //   });
 // }
 
-export class HeroDetailComponent3 {
+// export class HeroDetailComponent3 {
+//   heroForm: FormGroup;
+
+//   constructor(private fb: FormBuilder) {
+//     this.createForm();
+//   }
+
+//   createForm() {
+//     this.heroForm = this.fb.group({
+//       name: ['', Validators.required ],
+//     });
+//   }
+// }
+
+export class HeroDetailComponent4 {
   heroForm: FormGroup;
+  states = states;
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -36,6 +53,12 @@ export class HeroDetailComponent3 {
   createForm() {
     this.heroForm = this.fb.group({
       name: ['', Validators.required ],
+      street: '',
+      city: '',
+      state: '',
+      zip: '',
+      power: '',
+      sidekick: ''
     });
   }
 }
